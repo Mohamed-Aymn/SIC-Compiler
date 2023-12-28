e5be3a1c23dfd7645ac56707672732713b8b7200
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-12-28 16:50:57 +0200

    chore: sicxe formate 3 calculation

--- Src/SicxeObjectCodeGenerator/Libs/PassTwo.cs
+++ Src/SicxeObjectCodeGenerator/Libs/PassTwo.cs
@@ -1,6 +1,7 @@
 using Common.ArithmeticOps;
 using Common.Helpers;
 using Common.PassOne;
+using SicxeObjectCodeGenerator.ValueObjects;
 
 namespace SicxeObjectCodeGenerator.Libs;
 
@@ -44,86 +45,118 @@ public class PassTwo
                 continue;
             }
 
-            /**
-             * opcode calculation
-             * */
-            if (line.Instruction.Contains('+'))
-            {
-                line.Instruction = line.Instruction.Substring(1);
-            }
-            string opcode = Convertor.InstructionOpCode[line.Instruction];
+            // all of these should return decimal
+            string opcode = OpcodeHandler(line.Instruction);
+            Nixbpe nixbpe = NixbpeHandler(line.Instruction, line.Reference);
+            string thirdPart = ThirdPartHandler(labelTable, line.Reference, nixbpe.B, nixbpe.P);
 
-            /**
-             * bits
-             * */
-            string n = "0";
-            string i = "0";
-            string x = "0";
-            string b = "0";
-            string p = "0";
-            string e = "0";
-            // n and i
-            if (line.Reference.Contains('#') && !line.Reference.Contains('@'))
-            {
-                i = "1";
-            }
-            else if (!line.Reference.Contains('#') && line.Reference.Contains('@'))
-            {
-                n = "1";
-            }
-            else
-            {
-                n = "1";
-                i = "1";
-            }
-            // x
-            if (line.Reference.Contains(','))
-            {
-                x = "1";
-            }
-            // b and p
-            if (n == "1" && i == "1")
-            {
-                p = "1";
-            }
-            //e 
-            if (line.Instruction.Contains('+'))
-            {
-                e = "1";
-            }
-
-            /**
-             * Address
-             * */
-            string reference = "";
-            if (line.Reference.Contains(",X"))
-            {
-                // Console.Write(line.Reference);
-                reference = line.Reference.Substring(0, line.Reference.Length - 2);
-            }
-            else if (line.Reference.Contains('#') || line.Reference.Contains('@'))
-            {
-                reference = line.Reference.Substring(1);
-            }
-            string targetAddress = labelTable.LabelLocationFinder(reference);
-            if (targetAddress == "")
-            {
-                reference = BinaryOperations.ToHex(reference).PadLeft(12, '0');
-            }
-            else
-            {
-                string displacement = HexOperations.Addition(line.LocationCounter!, "3");
-                reference = HexOperations.Subtraction(targetAddress, displacement);
-            }
-
-            string objectCode = "";
-            objectCode += HexOperations.ToBinray(opcode[0].ToString());
-            objectCode += HexOperations.ToBinray(opcode[1].ToString())[0];
-            objectCode += HexOperations.ToBinray(opcode[1].ToString())[1];
-            objectCode += $"{n}{i}{x}{b}{p}{e}";
-            objectCode += reference;
+            // object code hex convertor
+            string objectCode = ObjectCodeGenerator(opcode, nixbpe, thirdPart);
 
             ObjectCodeList.AddLast(objectCode);
         }
+    }
+
+    private string ObjectCodeGenerator(string opcode, Nixbpe nixbpe, string thirdPart)
+    {
+        string binary = $"{opcode}{nixbpe.N}{nixbpe.I}{nixbpe.X}{nixbpe.B}{nixbpe.P}{nixbpe.E}{thirdPart}";
+        string result = "";
+
+        for (int i = 0; i < binary.Length / 4; i++)
+        {
+            string firstFourCharacters = binary.Substring(0, 4);
+            binary = binary.Substring(4);
+            result += BinaryOperations.ToHex(firstFourCharacters.ToString());
+        }
+
+        return (nixbpe.E == "1") ? result.PadRight(8, '0') : result.PadRight(6, '0');
+    }
+
+    private string ThirdPartHandler(LabelTable labelTable, string reference, string b, string p)
+    {
+        string label = "";
+        if (reference.Contains(",X"))
+        {
+            label = reference.Substring(0, reference.Length - 2);
+        }
+        else if (reference.Contains('#') || reference.Contains('@'))
+        {
+            label = reference.Substring(1);
+        }
+
+        string targetAddress = labelTable.LabelLocationFinder(reference);
+        string thirdPartResult = "";
+        if (targetAddress != "")
+        {
+            foreach (char character in targetAddress)
+            {
+                thirdPartResult += HexOperations.ToBinray(character.ToString());
+            }
+        }
+        else
+        {
+            foreach (char character in label)
+            {
+                thirdPartResult += HexOperations.ToBinray(character.ToString()).PadLeft(12, '0');
+            }
+        }
+
+        return thirdPartResult;
+    }
+
+    private Nixbpe NixbpeHandler(string instruction, string reference)
+    {
+        Nixbpe nixbpe = new();
+
+        // n and i
+        if (reference.Contains('#') && !reference.Contains('@'))
+        {
+            nixbpe.I = "1";
+        }
+        else if (!reference.Contains('#') && reference.Contains('@'))
+        {
+            nixbpe.N = "1";
+        }
+        else
+        {
+            nixbpe.N = "1";
+            nixbpe.I = "1";
+        }
+
+        // x
+        if (reference.Contains(','))
+        {
+            nixbpe.X = "1";
+        }
+
+        // b and p
+        if (nixbpe.N == "1" && nixbpe.I == "1")
+        {
+            nixbpe.P = "1";
+        }
+
+        //e 
+        if (instruction.Contains('+'))
+        {
+            nixbpe.E = "1";
+        }
+
+
+        return nixbpe;
+    }
+
+    private string OpcodeHandler(string instruction)
+    {
+        if (instruction.Contains('+'))
+        {
+            instruction = instruction.Substring(1);
+        }
+        string hexObjectCode = Convertor.InstructionOpCode[instruction];
+        string result = "";
+        foreach (char character in hexObjectCode)
+        {
+            result += HexOperations.ToBinray(character.ToString());
+        }
+        return result.Substring(0, result.Length - 2);
     }
 }
---
+++ Src/SicxeObjectCodeGenerator/ValueObjects/Nixbpe.cs
@@ -0,0 +1,11 @@
+namespace SicxeObjectCodeGenerator.ValueObjects;
+
+public record Nixbpe
+{
+    public string N { get; set; } = "0";
+    public string I { get; set; } = "0";
+    public string X { get; set; } = "0";
+    public string B { get; set; } = "0";
+    public string P { get; set; } = "0";
+    public string E { get; set; } = "0";
+}
