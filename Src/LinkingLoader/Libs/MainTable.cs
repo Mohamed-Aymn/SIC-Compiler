acb864bcd38dae1cd509f4f5d58b8f9b00777068
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-12-31 16:26:29 +0200

    chore: main table class structure

--- Src/LinkingLoader/Libs/MainTable.cs
+++ Src/LinkingLoader/Libs/MainTable.cs
@@ -1,20 +1,41 @@
+
 namespace LinkingLoader.Libs;
 
 public class MainTable
 {
-    // Table variable
+    public Dictionary<string, Dictionary<char, string>> Table { set; get; } = new();
 
     public MainTable(LinkedList<string[]> htes, LinkedList<ExternalProgramSymbols> externalSymbols)
     {
-        TableFiller();
-        TableModifier();
-    }
+        FillFromT(htes);
+        FillFromExternalSymboles(externalSymbols);
 
-    private void TableModifier()
+        TableModifier(htes, externalSymbols);
+    }
+    private void FillFromT(LinkedList<string[]> htes)
     {
+        foreach (string[] hte in htes)
+        {
+            bool isTMore = true;
+            int i = 1;
+            while (isTMore)
+            {
+                // type import logic here starting form hte[3]
+                if (hte[3 + i][0] != 'T')
+                {
+                    isTMore = false;
+                }
+
+            }
+        }
     }
 
-    private void TableFiller()
+    private void FillFromExternalSymboles(LinkedList<ExternalProgramSymbols> externalSymbols)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TableModifier(LinkedList<string[]> htes, LinkedList<ExternalProgramSymbols> externalSymbols)
     {
     }
 }
